4f65b94dbfc9d5f5a1a2924077ff54c6b1c260ea
Randy <randy708ochoa@gmail.com>
2023-06-08 19:15:44 -0600

    Botones agregados a las Cards de los productos y en la pagina Order ya no se repiten los productos

--- src/pages/MyOrder.jsx
+++ src/pages/MyOrder.jsx
@@ -7,12 +7,24 @@ export function MyOrder() {
         cartProducts,
     } = useContext(ShoppingContext)
 
+    const groupedProducts = [] 
+
+    cartProducts.map(product => {
+        const existingProduct = groupedProducts.find(pro => pro.title === product.title)
+
+        if (existingProduct) {
+            existingProduct.quantity += 1
+        } else {
+            groupedProducts.push({ ...product, quantity: 1 })
+        }
+    })
+
     return (
         <>
             <h1>MyOrder</h1>
 
             <div className="flex flex-wrap justify-center gap-3">
-                {cartProducts.map(product => (
+                {groupedProducts.map(product => (
                     <article 
                         key={product.id} 
                         className="grid grid-cols-[120px_1fr_1fr_1fr_40px] place-content-center place-items-center gap-2 w-9/12 h-24 max-w-3xl rounded-xl overflow-hidden bg-indigo-100"
@@ -24,7 +36,7 @@ export function MyOrder() {
                         />
 
                         <h2 className="flex-grow"> {product.title} </h2>
-                        <p className="flex-grow"> CANTIDAD DE PRODUCTOS </p>
+                        <p className="flex-grow"> CANTIDAD DE PRODUCTOS {product.quantity} </p>
                         <p className="flex-grow"> ${product.price} </p>
                         <AiFillDelete className="flex-grow text-3xl transition-colors duration-300 hover:text-red-600 cursor-pointer"/>
                     </article>
