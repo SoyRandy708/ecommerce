5e2a0b9cf5557807d8c3b9f51dffd70667ac89b5
Randy <randy708ochoa@gmail.com>
2023-07-29 15:31:19 -0600

    Added drop-dow product navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from "react";
 import { ShoppingContext } from "../context";
 import { NavLink } from "react-router-dom";
-import { AiOutlineShoppingCart } from "react-icons/ai"
+import { AiOutlineShoppingCart, AiOutlineDown } from "react-icons/ai"
 import { MdAccountCircle } from "react-icons/md"
 
 export function Navbar() {
@@ -12,17 +12,14 @@ export function Navbar() {
         setSearchByCategory,
     } = useContext(ShoppingContext)
 
-    const activeStyle = "underline underline-offset-4"
-
     const menuLeft = [
-        {to: "/", texto: "Home"},
-        {to: "/Products/All", texto: "All"},
-        {to: "/Products/Favorites", texto: "Favorites"},
-        {to: "/Products/Category/Smartphones", texto: "Smartphones"},
-        {to: "/Products/Category/Laptops", texto: "Laptops"},
-        {to: "/Products/Category/Fragrances", texto: "Fragrances"},
-        {to: "/Products/Category/Skincare", texto: "Skincare"},
-        {to: "/Products/Category/Groceries", texto: "Groceries"},
+        {to: "/Products/All", text: "All"},
+        {to: "/Products/Favorites", text: "Favorites"},
+        {to: "/Products/Category/Smartphones", text: "Smartphones"},
+        {to: "/Products/Category/Laptops", text: "Laptops"},
+        {to: "/Products/Category/Fragrances", text: "Fragrances"},
+        {to: "/Products/Category/Skincare", text: "Skincare"},
+        {to: "/Products/Category/Groceries", text: "Groceries"},
     ]
 
     const selectCategory = () => {
@@ -46,40 +43,65 @@ export function Navbar() {
     }, [])
 
     return (
-        <nav className="w-full h-[60px] flex justify-between items-center fixed top-0 z-10 py-5 px-8 text-base bg-violet-200"> 
-            <ul className="flex items-center gap-5">
-                {menuLeft.map(link => (
-                    <li key={link.texto} >
-                        <NavLink 
-                            to={link.to}
-                            onClick={() => selectCategory()}
-                            className={({isActive}) => isActive ? activeStyle : ""}
+        <nav className="w-full h-[50px] flex justify-between items-center fixed top-0 z-10 text-base bg-violet-200"> 
+            <nav className="flex justify-between w-full max-w-5xl h-full m-auto py-2 px-8">
+                <ul className="flex items-center gap-5">
+                    <li>
+                        <NavLink
+                            to={"/"}
+                            className={({isActive}) => isActive ? "font-bold p-2.5 leading-[30px]" : "p-2.5 leading-[30px]"}
                         >
-                            {link.texto}
+                            Home
                         </NavLink>
                     </li>
-                ))}             
-            </ul>
 
-            <ul className="flex items-center gap-5">
-                <li>
-                    <NavLink
-                        to={"/Account"}
-                    >
-                        <MdAccountCircle  className="inline-block w-7 h-7 mr-1" />
-                        {signIn ? account?.username : ""}
-                    </NavLink>
-                </li>
+                    <li className="group">
+                        <NavLink
+                            to={"/Products/All"}
+                            onClick={() => selectCategory()}
+                            className="p-2.5 leading-[30px] cursor-pointer"
+                        >
+                            Products    
+                        </NavLink>
+                            
+                        <ul className="flex flex-col gap-2 absolute top-full w-auto invisible overflow-hidden p-3 rounded-b-lg bg-cyan-200 origin-top scale-y-0 transition-all duration-300 group-hover:scale-y-100 group-hover:visible">
+                            {menuLeft.map(link => (
+                                <li key={link.text} >
+                                    <NavLink 
+                                        to={link.to}
+                                        onClick={() => selectCategory()}
+                                        className={({isActive}) => isActive ? "inline-block w-full p-2 font-bold" : "inline-block w-full p-2"}
+                                    >
+                                        {link.text}
+                                    </NavLink>
+                                </li>
+                            ))}             
+                        </ul>
+                    </li>
+                </ul>
 
-                <li>
-                    <NavLink
-                        to={"/MyOrder"}
-                    >
-                        <AiOutlineShoppingCart className="inline-block w-6 h-6 mr-1"/> 
-                        {cartProducts.length}
-                    </NavLink>
-                </li>
-            </ul>
+                <ul className="flex items-center gap-5">
+                    <li>
+                        <NavLink
+                            to={"/Account"}
+                            className="p-2.5 leading-[30px]"
+                        >
+                            <MdAccountCircle  className="inline-block w-7 h-7 mr-1" />
+                            {signIn ? account?.username : ""}
+                        </NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink
+                            to={"/MyOrder"}
+                            className="p-2.5 leading-[30px]"
+                        >
+                            <AiOutlineShoppingCart className="inline-block w-6 h-6 mr-1"/> 
+                            {cartProducts.length}
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
         </nav>
     )
 }
